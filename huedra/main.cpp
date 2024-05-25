e86a128d8e2ec558af52bb652d67617804c6a98f
William Kamnert <wille.kamnert@gmail.com>
2024-05-25 20:18:38 +0200

    Basic types and C++23

--- huedra/main.cpp
+++ huedra/main.cpp
@@ -1,6 +1,11 @@
+#include <bit>
+#include <cstdint>
 #include <iostream>
+
+#include "types.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    huedra::u64 num = 64;
+    std::cout << "Hello World! " << std::byteswap(num) << "\n";
 }
