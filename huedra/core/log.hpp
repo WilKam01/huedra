88abe08a6a6683b9d344e3e6c8fb30fe1734c301
William Kamnert <wille.kamnert@gmail.com>
2026-09-24 06:49:27 +0200

    Remove old commented log

--- huedra/core/log.hpp
+++ huedra/core/log.hpp
@@ -4,9 +4,7 @@
 #include <iostream>
 #include <source_location>
 
-namespace huedra {
-
-namespace log {
+namespace huedra::log {
 
 enum class Level
 {
@@ -139,63 +137,4 @@ LOG_IMPL(fatal, FATAL)
 
 #undef LOG_IMPL
 
-} // namespace log
-
-/*enum class LogLevel
-{
-    D_INFO, // Debug info
-    INFO,
-    WARNING,
-    ERR
-};
-
-void log(LogLevel level, const char* formatStr, auto&&... args)
-{
-#ifndef DEBUG
-    if (level == LogLevel::D_INFO)
-    {
-        return;
-    }
-#endif
-
-    const char* levelStr{nullptr};
-    switch (level)
-    {
-    case LogLevel::D_INFO:
-        levelStr = "DEBUG INFO";
-        break;
-    case LogLevel::INFO:
-        levelStr = "INFO";
-        break;
-    case LogLevel::WARNING:
-        levelStr = "WARNING";
-        break;
-    case LogLevel::ERR:
-        levelStr = "ERROR";
-        break;
-    default:
-        levelStr = "UNKNOWN";
-        break;
-    }
-
-    try
-    {
-        std::string formattedMessage = std::vformat(formatStr, std::make_format_args(args...));
-        std::cout << "[" << levelStr << "] " << formattedMessage << "\n";
-    }
-    catch (const std::format_error& error)
-    {
-        std::cout << "log(): " << error.what() << "\n";
-    }
-    catch (const std::exception& error)
-    {
-        std::cout << "log(): Unexpected error: " << error.what() << "\n";
-    }
-
-    if (level == LogLevel::ERR)
-    {
-        exit(1);
-    }
-}*/
-
-} // namespace huedra
+} // namespace huedra::log
